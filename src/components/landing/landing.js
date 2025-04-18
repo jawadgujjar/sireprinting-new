70e8ecc3170b894052cdf3f359f63fa55f598d35
usamaahma <usamaahmad087@gmail.com>
2025-04-18 17:01:37 +0500

    login,search

--- src/components/landing/landing.js
+++ src/components/landing/landing.js
@@ -8,7 +8,7 @@ const Landing = () => {
     <div className="landing-page">
       <div className="landing-content">
         <h1 class="premium-heading">Welcome to the Future of Packaging</h1>
-        <p>
+        <p className="p-tag-landing">
           Unleash your wild ideas with custom printed packaging. Elevate your
           brand with packaging that drives your customers crazy.
         </p>
