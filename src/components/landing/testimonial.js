bc40cf827d06800f2120728db90c5d92a12604f1
usamaahma <usamaahmad087@gmail.com>
2025-06-19 15:17:01 +0500

    google reviews updated

--- src/components/landing/testimonial.js
+++ src/components/landing/testimonial.js
@@ -3,31 +3,41 @@ import "./testimonial.css";
 
 const Testimonial = () => {
   useEffect(() => {
-    // Load script and add mutation observer to remove branding
+    // Load Featurable script
     const script = document.createElement("script");
     script.src = "https://featurable.com/assets/bundle.js";
     script.defer = true;
     script.charset = "UTF-8";
     document.body.appendChild(script);
 
-    // Observer to remove branding as soon as it appears
-    const observer = new MutationObserver((mutations) => {
-      const branding = document.querySelector(
-        '.powered-by-featurable, [href*="featurable"]'
+    // Remove branding repeatedly
+    const removeBranding = () => {
+      const brandingEls = document.querySelectorAll(
+        '.powered-by-featurable, [href*="featurable"], .Branding-module__inline--DrMTH'
       );
-      if (branding) {
-        branding.remove();
-      }
-    });
+      brandingEls.forEach((el) => {
+        el.style.display = "none";
+        el.remove();
+      });
+    };
 
-    observer.observe(document.body, {
-      childList: true,
-      subtree: true,
-    });
+    // Run initially after a small delay
+    setTimeout(removeBranding, 2000);
 
+    // Keep checking using interval (in case it's loaded later)
+    const interval = setInterval(() => {
+      removeBranding();
+    }, 1500);
+
+    // Also observe for changes in DOM
+    const observer = new MutationObserver(removeBranding);
+    observer.observe(document.body, { childList: true, subtree: true });
+
+    // Clean up
     return () => {
       document.body.removeChild(script);
       observer.disconnect();
+      clearInterval(interval);
     };
   }, []);
 
@@ -41,12 +51,16 @@ const Testimonial = () => {
       </div>
 
       <div
+        style={{
+          maxHeight: "100px",
+          height: "100px",
+          overflow: "hidden",
+          position: "relative",
+        }}
         id="featurable-d4ac21d0-a964-4f3c-8665-528e6b784256"
         data-featurable-async
         className="featurable-widget-container"
-      >
-        <div className="widget-loading">Loading reviews...</div>
-      </div>
+      ></div>
     </div>
   );
 };
